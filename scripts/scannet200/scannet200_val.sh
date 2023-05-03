59949d1b657a7141f758c07541588e5e6f80e66a
salwaalkhatib <salwa.al.khatib22@gmail.com>
2023-05-03 18:38:39 +0400

    update

--- scripts/scannet200/scannet200_val.sh
+++ scripts/scannet200/scannet200_val.sh
@@ -7,7 +7,7 @@ CURR_QUERY=150
 
 # TRAIN
 python main_instance_segmentation.py \
-general.experiment_name="scannet200_val" \
+general.experiment_name="scannet200_val_test" \
 general.project_name="scannet200" \
 data/datasets=scannet200 \
 general.num_targets=201 \
@@ -16,17 +16,17 @@ general.eval_on_segments=true \
 general.train_on_segments=true
 
 # TEST
-python main_instance_segmentation.py \
-general.experiment_name="scannet200_val_query_${CURR_QUERY}_topk_${CURR_TOPK}_dbscan_${CURR_DBSCAN}" \
-general.project_name="scannet200_eval" \
-general.checkpoint="checkpoints/scannet200/scannet200_val.ckpt" \
-data/datasets=scannet200 \
-general.num_targets=201 \
-data.num_labels=200 \
-general.eval_on_segments=true \
-general.train_on_segments=true \
-general.train_mode=false \
-model.num_queries=${CURR_QUERY} \
-general.topk_per_image=${CURR_TOPK} \
-general.use_dbscan=true \
-general.dbscan_eps=${CURR_DBSCAN}
+# python main_instance_segmentation.py \
+# general.experiment_name="scannet200_val_query_${CURR_QUERY}_topk_${CURR_TOPK}_dbscan_${CURR_DBSCAN}" \
+# general.project_name="scannet200_eval" \
+# general.checkpoint="checkpoints/scannet200/scannet200_val.ckpt" \
+# data/datasets=scannet200 \
+# general.num_targets=201 \
+# data.num_labels=200 \
+# general.eval_on_segments=true \
+# general.train_on_segments=true \
+# general.train_mode=false \
+# model.num_queries=${CURR_QUERY} \
+# general.topk_per_image=${CURR_TOPK} \
+# general.use_dbscan=true \
+# general.dbscan_eps=${CURR_DBSCAN}
